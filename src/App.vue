14d0297d1437a574760a3be30fbbc30efce255cc
Peter van Heusden <pvh@sanbi.ac.za>
2019-07-28 00:39:30 +0200

    total redesign

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,70 @@
 <template>
-  <div id="app" class="container">
-    <div class="row">
-      <div class="col-2">
-        <h1 class="bg-dark text-white" style="height: 400px">Hello Sqoo</h1>
-      </div>
-      <div id="portfolio" class="col-8">
-        <h3>Portfolio</h3>
-      </div>
-    </div>
-  </div>
+    <v-app>
+        <!--    <v-app-bar app>-->
+        <!--      <v-toolbar-title class="headline text-uppercase">-->
+        <!--        <span>Vuetify</span>-->
+        <!--        <span class="font-weight-light">MATERIAL DESIGN</span>-->
+        <!--      </v-toolbar-title>-->
+        <!--      <v-spacer></v-spacer>-->
+        <!--      <v-btn-->
+        <!--        text-->
+        <!--        href="https://github.com/vuetifyjs/vuetify/releases/latest"-->
+        <!--        target="_blank"-->
+        <!--      >-->
+        <!--        <span class="mr-2">Latest Release</span>-->
+        <!--      </v-btn>-->
+        <!--    </v-app-bar>-->
+
+        <v-content>
+            <v-container grid-list-md>
+                <v-layout>
+                    <v-flex v-for="i in 2" :key="`${i}`" xs6>
+                        <v-img :src="getImgUrl(image_names[i-1])" class="elevation-10" v-on:click="expandImage(i)"/>
+                    </v-flex>
+                    <!--                <v-flex xs6>-->
+                    <!--                    <v-img src="./assets/artwork2.jpg" class="elevation-10" @click="expandImage"/>-->
+                    <!--                </v-flex>-->
+                    <v-flex xs3>
+                        <span class="display-3">Sqoo Site</span>
+                        <p class="display-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+            <v-overlay :value="overlay">
+                <v-container grid-list-sm>
+                    <v-layout>
+                        <v-flex xs12>
+                            <v-img v-if="current" :src="getImgUrl(image_names[current-1])" @click="overlay = false" class="elevation-10" />
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+            </v-overlay>
+
+        </v-content>
+    </v-app>
 </template>
 
 <script>
 
-export default {
-  name: 'app',
-  components: {
+    // const images = require.context('@/assets/img/covers', false, /\.png$|\.jpg$/)
 
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+        },
+        data: () => ({
+            overlay: false,
+            current: null,
+            image_names: ['artwork.jpg', 'artwork2.jpg']
+        }),
+        methods: {
+            expandImage: function (num) {
+                this.overlay = true
+                this.current = num;
+            },
+            getImgUrl(pic) {
+                return require('./assets/'+pic)
+            }
+        }
+    };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-  body {
-    background-image: url("assets/background.png");
-    background-repeat: repeat;
-  }
-</style>
